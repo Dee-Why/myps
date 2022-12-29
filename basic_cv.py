4ddc8e5e35150cf6d8bb9a00b9ab1b97d699eb04
Dee-Why <by_liumingchao@pku.edu.cn>
2022-12-29 15:09:08 +0800

    modify_shadow

--- basic_cv.py
+++ basic_cv.py
@@ -5,22 +5,26 @@ import numpy as np
 
 class PSShadowHighlight:
     """
-    色阶调整
+    色阶调整 默认输入图片为opencv风格的np.array
     """
-    def __init__(self, image):
-        self.shadows_light = 50
+    def __init__(self, image, parameter=50, threshold_percentile=90):
+        self.parameter = parameter
+        self.threshold_percentile = threshold_percentile
 
+        # 单位化图片
         img = image.astype(float)/255.0
 
+        # 取不同通道
         srcR = img[:, :, 2]
         srcG = img[:, :, 1]
         srcB = img[:, :, 0]
         srcGray = 0.299 * srcR + 0.587 * srcG + 0.114 * srcB
 
-        # 阴影选取
+        # 计算明度矩阵
         luminance = (1-srcGray) * (1-srcGray)
 
-        self.maskThreshold = np.mean(luminance)
+        self.maskThreshold = np.percentile(luminance, self.threshold_percentile)
+        # 将明度小于均值的部分视为阴影区，这里mask里1为明亮区，0为阴影区
         mask = luminance > self.maskThreshold
         imgRow = np.size(img, 0)
         imgCol = np.size(img, 1)
@@ -35,12 +39,12 @@ class PSShadowHighlight:
         self.brightnessRate = np.zeros([imgRow, imgCol, 3], dtype=float)
 
     def adjust_image(self, img):
-        maxRate = 4
-        brightness = (self.shadows_light / 100.0 - 0.0001) / maxRate
-        midtones = 1 + maxRate * brightness
+        maxRate = 0.25
+        midTones = 1 + self.parameter / 100.0
+        brightness = (self.parameter / 100.0) * maxRate
 
-        self.midtonesRate[self.rgbMask] = midtones
-        self.midtonesRate[~self.rgbMask] = (midtones-1.0) / self.maskThreshold * self.rgbLuminance[~self.rgbMask] + 1.0
+        self.midtonesRate[self.rgbMask] = midTones
+        self.midtonesRate[~self.rgbMask] = (midTones-1.0) / self.maskThreshold * self.rgbLuminance[~self.rgbMask] + 1.0
 
         self.brightnessRate[self.rgbMask] = brightness
         self.brightnessRate[~self.rgbMask] = (1 / self.maskThreshold * self.rgbLuminance[~self.rgbMask]) * brightness
@@ -56,19 +60,25 @@ class PSShadowHighlight:
 
 
 def ps_shadow_highlight_adjust_and_save_img(psSH, origin_image):
-    psSH.shadows_light = 50
+    psSH.parameter = 50
     image = psSH.adjust_image(origin_image)
     cv.imwrite('py_sh_out_01.png', image)
 
 
-def ps_shadow_highlight_adjust(path):
+def ps_shadow_highlight_adjust(path, outPath):
     """
     阴影提亮调整
     """
     origin_image = cv.imread(path)
     psSH = PSShadowHighlight(origin_image)
     image = psSH.adjust_image(origin_image)
-    cv.imwrite('test_output.png', image)
+    cv.imwrite(outPath, image)
+
+
+def modify_shadow(cvimage, factor, shadow_percentile):
+    psSH = PSShadowHighlight(cvimage, factor, shadow_percentile)
+    image = psSH.adjust_image(cvimage)
+    return image
 
 
 if __name__ == '__main__':
@@ -80,5 +90,6 @@ if __name__ == '__main__':
         print("参数错误，没有检测到图片路径")
         sys.exit(-1)
     img_path = sys.argv[1]
-    print("img_path Params:", img_path)
-    ps_shadow_highlight_adjust(img_path)
+    out_path = sys.argv[2]
+    print("img_path Params:", img_path, "out_path:", out_path)
+    ps_shadow_highlight_adjust(img_path, out_path)
