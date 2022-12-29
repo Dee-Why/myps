7d1916c2066901b43ad763d3698aaef241c7bf96
Dee-Why <by_liumingchao@pku.edu.cn>
2022-12-29 20:48:37 +0800

    psHighlight

--- basic_cv.py
+++ basic_cv.py
@@ -81,6 +81,68 @@ def modify_shadow(cvimage, factor, shadow_percentile):
     return image
 
 
+class PSHighlight:
+    """
+    色阶调整 默认输入图片为opencv风格的np.array
+    """
+    def __init__(self, image, parameter=50, threshold_percentile=90):
+        self.parameter = parameter
+        self.threshold_percentile = threshold_percentile
+
+        # 单位化图片
+        img = image.astype(float)/255.0
+
+        # 取不同通道
+        srcR = img[:, :, 2]
+        srcG = img[:, :, 1]
+        srcB = img[:, :, 0]
+        srcGray = 0.299 * srcR + 0.587 * srcG + 0.114 * srcB
+
+        # 计算明度矩阵
+        luminance = (1-srcGray) * (1-srcGray)
+
+        self.maskThreshold = np.percentile(luminance, self.threshold_percentile)
+        # 将明度小于均值的部分视为阴影区，这里mask里1为明亮区，0为阴影区
+        mask = luminance > self.maskThreshold
+        imgRow = np.size(img, 0)
+        imgCol = np.size(img, 1)
+
+        self.rgbMask = np.zeros([imgRow, imgCol, 3], dtype=bool)
+        self.rgbMask[:, :, 0] = self.rgbMask[:, :, 1] = self.rgbMask[:, :, 2] = mask
+
+        self.rgbLuminance = np.zeros([imgRow, imgCol, 3], dtype=float)
+        self.rgbLuminance[:, :, 0] = self.rgbLuminance[:, :, 1] = self.rgbLuminance[:, :, 2] = luminance
+
+        self.midtonesRate = np.zeros([imgRow, imgCol, 3], dtype=float)
+        self.brightnessRate = np.zeros([imgRow, imgCol, 3], dtype=float)
+
+    def adjust_image(self, img):
+        maxRate = 0.25
+        midTones = 1 + self.parameter / 100.0
+        brightness = (self.parameter / 100.0) * maxRate
+
+        self.midtonesRate[~self.rgbMask] = midTones
+        self.midtonesRate[self.rgbMask] = (midTones-1.0) / self.maskThreshold * self.rgbLuminance[self.rgbMask] + 1.0
+
+        self.brightnessRate[~self.rgbMask] = brightness
+        self.brightnessRate[self.rgbMask] = (1 / self.maskThreshold * self.rgbLuminance[self.rgbMask]) * brightness
+
+        outImg = 255 * np.power(img / 255.0, 1.0 / self.midtonesRate) * (1.0 / (1 - self.brightnessRate))
+
+        img = outImg
+        img[img < 0] = 0
+        img[img > 255] = 255
+
+        img = img.astype(np.uint8)
+        return img
+
+
+def modify_highlight(cvimage, factor, shadow_percentile):
+    psSH = PSHighlight(cvimage, factor, shadow_percentile)
+    image = psSH.adjust_image(cvimage)
+    return image
+
+
 def modify_color_temperature(img, factor):
     imgB = img[:, :, 0]
     imgG = img[:, :, 1]
