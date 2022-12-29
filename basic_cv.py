e2e97e46d64155747d6ae3ba469ba1920aaa29e1
Dee-Why <by_liumingchao@pku.edu.cn>
2022-12-29 10:52:46 +0800

    CSDN

---
+++ basic_cv.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import cv2 as cv
+import numpy as np
+
+class PSShadowHighlight:
+    """
+    色阶调整
+    """
+    def __init__(self, image):
+        self.shadows_light = 50
+
+        img = image.astype(float)/255.0
+
+        srcR = img[:, :, 2]
+        srcG = img[:, :, 1]
+        srcB = img[:, :, 0]
+        srcGray = 0.299 * srcR + 0.587 * srcG + 0.114 * srcB
+
+        # 阴影选取
+        luminance = (1-srcGray) * (1-srcGray)
+
+        self.maskThreshold = np.mean(luminance)
+        mask = luminance > self.maskThreshold
+        imgRow = np.size(img, 0)
+        imgCol = np.size(img, 1)
+
+        self.rgbMask = np.zeros([imgRow, imgCol, 3], dtype=bool)
+        self.rgbMask[:, :, 0] = self.rgbMask[:, :, 1] = self.rgbMask[:, :, 2] = mask
+
+        self.rgbLuminance = np.zeros([imgRow, imgCol, 3], dtype=float)
+        self.rgbLuminance[:, :, 0] = self.rgbLuminance[:, :, 1] = self.rgbLuminance[:, :, 2] = luminance
+
+        self.midtonesRate = np.zeros([imgRow, imgCol, 3], dtype=float)
+        self.brightnessRate = np.zeros([imgRow, imgCol, 3], dtype=float)
+
+    def adjust_image(self, img):
+        maxRate = 4
+        brightness = (self.shadows_light / 100.0 - 0.0001) / maxRate
+        midtones = 1 + maxRate * brightness
+
+        self.midtonesRate[self.rgbMask] = midtones
+        self.midtonesRate[~self.rgbMask] = (midtones-1.0) / self.maskThreshold * self.rgbLuminance[~self.rgbMask] + 1.0
+
+        self.brightnessRate[self.rgbMask] = brightness
+        self.brightnessRate[~self.rgbMask] = (1 / self.maskThreshold * self.rgbLuminance[~self.rgbMask]) * brightness
+
+        outImg = 255 * np.power(img / 255.0, 1.0 / self.midtonesRate) * (1.0 / (1 - self.brightnessRate))
+
+        img = outImg
+        img[img < 0] = 0
+        img[img > 255] = 255
+
+        img = img.astype(np.uint8)
+        return img
+
+
+def ps_shadow_highlight_adjust_and_save_img(psSH, origin_image):
+    psSH.shadows_light = 50
+    image = psSH.adjust_image(origin_image)
+    cv.imwrite('py_sh_out_01.png', image)
+
+
+def ps_shadow_highlight_adjust(path):
+    """
+    阴影提亮调整
+    """
+    origin_image = cv.imread(path)
+
+    psSH = PSShadowHighlight(origin_image)
+
+    def update_shadows_light(x):
+        psSH.shadows_light = x
+
+    title = "ShadowHighlight"
+    cv.namedWindow(title, cv.WINDOW_NORMAL)
+    cv.resizeWindow(title, 800, 600)
+    cv.moveWindow(title, 0, 0)
+
+    option_title = "Option"
+    cv.namedWindow(option_title, cv.WINDOW_NORMAL)
+    cv.resizeWindow(option_title, 400, 20)
+    cv.moveWindow(option_title, 0, 630)
+    cv.createTrackbar('shadows_light', option_title, psSH.shadows_light, 100, update_shadows_light)
+
+    while True:
+        image = psSH.adjust_image(origin_image)
+        cv.imshow(title, image)
+        if cv.waitKey(1) == ord('q'):
+            break
+    cv.destroyAllWindows()
+    cv.imwrite('test_output.png', image)
+
+
+if __name__ == '__main__':
+    """
+    usage:
+    python basic_cv.py test.jpg
+    """
+    if len(sys.argv) == 1:
+        print("参数错误，没有检测到图片路径")
+        sys.exit(-1)
+    img_path = sys.argv[1]
+    print("img_path Params:", img_path)
+    ps_shadow_highlight_adjust(img_path)
--- utils.py
+++ utils.py
@@ -1,18 +1,32 @@
 import cv2 as cv
 import sys
+import numpy as np
+from PIL import Image
 
 
-def read_image(filename):
+def read_image_cv(filename):
     img = cv.imread(filename, cv.IMREAD_UNCHANGED)  # IMREAD_COLOR, IMREAD_GREYSCALE
     if img is None:
         sys.exit("Could not read the image.")
     return img
 
+# def show_image_cv(img):
+#     cv.imshow("Display window", img)
+#     k = cv.waitKey(0)
 
-def show_image(img):
-    cv.imshow("Display window", img)
-    k = cv.waitKey(0)
 
-
-def save_image(img, filename):
+def save_image_cv(img, filename):
     cv.imwrite(filename, img)
+
+
+def cvarray_to_pilImage(opencv_array):
+    color_coverted = cv.cvtColor(opencv_array, cv.COLOR_BGR2RGB)
+    im_converted = Image.fromarray(color_coverted, mode="RGB")
+    return im_converted
+
+
+def pilImage_to_cvarray(pilImage):
+    arr = np.asarray(pilImage)
+    converted_arr = cv.cvtColor(arr, cv.COLOR_RGB2BGR)
+    return converted_arr
+
